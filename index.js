97370dbd06b340f0218dfa3e8d60658d25f638c2
Roger Arruda <roger@devsquad.com>
2024-09-27 10:27:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import VueErrorHandler from './src/vueErrorHandler';
-import NuxtErrorHandler from './src/nuxtErrorHandler';
 import Debugmate from "./src/debugmate";
 
-export { VueErrorHandler, NuxtErrorHandler, Debugmate };
+export { Debugmate };
